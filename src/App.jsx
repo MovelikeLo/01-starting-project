1b346bf6e45d41bd4c118e8e77c8076fc70fb60a
MovelikeLo <loicrobert640@gmail.com>
2026-04-06 12:14:54 -0400

    customs components and dynamic data

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import reactImg from './assets/react-core-concepts.png';
+
+
+function Header() {
+  const reactDscriptions = ['Fundamental', 'Crucial', 'Core'];
+  function getRandomint(max) {
+    return Math.floor(Math.random() * (max + 1));
+  }
+  const description = reactDscriptions[getRandomint(2)];
+  return (
+     <header>
+      <img src={reactImg} alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+           {description} React concepts you will need for almost any app you are going to build.
+        </p>
+      </header>
+  );
+}
+
+function App() {
+  return (
+    <div>
+   <Header/>
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
